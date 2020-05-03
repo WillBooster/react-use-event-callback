690a6e98f006c0d3980cd0a0a115967fcd1c5cc4
Ryosuke Ishizue <ryoskate@gmail.com>
2020-05-03 09:45:23 +0900

    feat: add useEventCallback()

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-import React from 'react';
+import { useCallback, useLayoutEffect, useRef } from 'react';
 
-export function useEventCallback(): void {}
+type Fn<ARGS extends any[], R> = (...args: ARGS) => R;
+
+export function useEventCallback<A extends any[], R>(fn: Fn<A, R>): Fn<A, R> {
+  const ref = useRef<Fn<A, R>>(fn);
+  useLayoutEffect(() => {
+    ref.current = fn;
+  });
+  return useCallback((...args) => ref.current(...args), []);
+}
