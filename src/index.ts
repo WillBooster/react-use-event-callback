cd53f9a430b925e624772c85408357dc4eef4efa
Sakamoto, Kazunori <exkazuu@gmail.com>
2020-05-04 17:40:43 +0900

    use useImperativeHandle instead of useLayoutEffect

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import { useCallback, useLayoutEffect, useRef } from 'react';
+import { useCallback, useImperativeHandle, useRef } from 'react';
 
-type Fn<ARGS extends any[], R> = (...args: ARGS) => R;
-
-export function useEventCallback<A extends any[], R>(fn: Fn<A, R>): Fn<A, R> {
-  const ref = useRef<Fn<A, R>>(fn);
-  useLayoutEffect(() => {
-    ref.current = fn;
-  });
-  return useCallback((...args) => ref.current(...args), []);
+/**
+ * Return a memorized callback of the given callback.
+ * The memorized callback cannot be used in concurrent mode and useLayoutEffect in a descendant component
+ * c.f. https://github.com/facebook/react/issues/14099#issuecomment-440013892
+ * c.f. https://github.com/facebook/react/issues/14099#issuecomment-499781277
+ * c.f. https://github.com/facebook/react/issues/14099#issuecomment-569027159
+ * @param callback
+ */
+export function useEventCallback<T extends (...args: any[]) => any>(callback: T): T {
+  const ref = useRef<T>(callback);
+  useImperativeHandle(ref, () => callback);
+  return useCallback((...args: any[]) => (void 0, ref.current)(...args), []) as T;
 }
