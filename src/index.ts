cfe3b2e1f50d2a8fe2bb41cefadbf7c5be901e4e
Sakamoto, Kazunori <exkazuu@gmail.com>
2020-05-04 17:44:57 +0900

    Add comments

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import { useCallback, useImperativeHandle, useRef } from 'react';
 
 /**
  * Return a memorized callback of the given callback.
- * The memorized callback cannot be used in concurrent mode and useLayoutEffect in a descendant component
+ * The memorized callback cannot be used in concurrent mode and `useLayoutEffect` of a descendant component.
+ * When you use the memorized callback in `useLayoutEffect` of a descendant component,
+ * an error where `ref.current` is `null` occurs. So you can recognize such a bug.
+ *
  * c.f. https://github.com/facebook/react/issues/14099#issuecomment-440013892
  * c.f. https://github.com/facebook/react/issues/14099#issuecomment-499781277
  * c.f. https://github.com/facebook/react/issues/14099#issuecomment-569027159
